577b33b533619824310a2c7f3fc0601442244e3c
Erik Kronberg <erik.a.kronberg@gmail.com>
2011-12-19 18:52:14 +0100

    Added migration/database tables to project
    migration class wishlistd.models.create
    
    can be run with lein run -m "wishlistd.models.create"

---
+++ src/wishlistd/models/create.clj
@@ -0,0 +1,37 @@
+(ns wishlistd.models.create
+  (:require [clojure.java.jdbc :as sql]))
+
+(def db {:classname "org.postgresql.Driver"
+		 :subprotocol "postgresql"
+		 :subname "//localhost:5432/cwl"
+		 :user "cwl"
+		 :password "wish"})
+
+(defn create-wishlist []
+  (sql/with-connection db
+    (sql/create-table :wishlist
+    	              [:id :serial "PRIMARY KEY"]
+	                  [:title :varchar "NOT NULL"]
+	                  [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"])))
+
+(defn create-wish []
+  (sql/with-connection db
+	(sql/create-table :wish
+		              [:id :serial "PRIMARY KEY"]
+		              [:description :varchar "NOT NULL"]
+		              [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]
+		              [:url :varchar]
+		              [:wishlist :int]
+		              ["constraint fk_wish_wishlist foreign key(wishlist) references Wishlist(id)"])))
+
+(defn drop-tables []
+  (sql/with-connection db
+	(sql/drop-table :wish)
+	(sql/drop-table :wishlist)))
+
+(defn -main []
+  (print "Migrating database...") (flush)
+  (drop-tables)
+  (create-wishlist)
+  (create-wish)
+  (println " done"))
