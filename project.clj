1406b83e34d6c175bdc8c4ae20ee293a9f384136
Erik Kronberg <erik.a.kronberg@gmail.com>
2011-12-19 17:48:40 +0100

    Added JDBC and PostreSQL dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject wishlistd "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.2.1"]
-                           [noir "1.1.0"]]
+                           [noir "1.1.0"]
+                           [postgresql/postgresql "8.4-702.jdbc4"]
+                 		   [org.clojure/java.jdbc "0.1.1"]]
+            :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
             :main wishlistd.server)
 
