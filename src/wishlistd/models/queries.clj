c07a46a99afed15aabc7ec8804cc4c82591da12b
Erik Kronberg <erik.a.kronberg@gmail.com>
2011-12-20 09:10:51 +0100

    Started making queries.

---
+++ src/wishlistd/models/queries.clj
@@ -0,0 +1,27 @@
+(ns wishlistd.models.queries
+  (:require [clojure.java.jdbc :as sql]))
+
+(def db {:classname "org.postgresql.Driver"
+		 :subprotocol "postgresql"
+		 :subname "//localhost:5432/cwl"
+		 :user "cwl"
+		 :password "wish"})
+;wishlist
+;[:id :serial "PRIMARY KEY"]
+;[:title :varchar "NOT NULL"]
+;[:code :varchar "NOT NULL"]
+;[:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"])))
+
+;wish
+;[:id :serial "PRIMARY KEY"]
+;[:description :varchar "NOT NULL"]
+;[:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]
+;[:url :varchar]
+;[:wishlist :int]
+;["constraint fk_wish_wishlist foreign key(wishlist) references Wishlist(id)"])))
+
+(defn get-wishlist [code]
+  (sql/with-connection db
+	(sql/with-query-results results
+      ["select * from wishlist where code=?" code]
+      (into [] results))))
