2ee035086d599b6a8103f7b464c4228bf9028ef9
Erik Kronberg <erik.a.kronberg@gmail.com>
2011-12-24 15:08:42 +0100

    Renamed view welcome.clj to wishlist.clj

--- src/wishlistd/views/wishlist.clj
+++ src/wishlistd/views/wishlist.clj
@@ -1,6 +1,5 @@
-(ns wishlistd.views.welcome
-  (:require [wishlistd.views.common :as common]
-            [noir.content.pages :as pages])
+(ns wishlistd.views.wishlist
+  (:require [wishlistd.views.common :as common])
   (:use noir.core
         hiccup.core
         hiccup.page-helpers))
