87edaafaab4f0c46ee033260cd26e3df789b6bda
Erik Kronberg <erik.a.kronberg@gmail.com>
2011-12-27 00:50:56 +0100

    Simplified code generating class

--- src/wishlistd/code.clj
+++ src/wishlistd/code.clj
@@ -2,13 +2,10 @@
 
 (def letters ["A","B","C","D","E","F","G","H","I","J","K"])
 
-(defn as-string [id]
-  (format "%03d" id))
-
 (defn generate-code [{:keys [id]}]
   (apply str 
     (map 
       (comp 
         #(letters %) 
         #(Integer/parseInt (str %))) 
-      (as-string id))))
+      (format "%03d" id))))
