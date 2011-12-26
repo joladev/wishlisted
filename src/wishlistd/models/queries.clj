14ea673f114a8c561841a56cd24ac75d2beed64d
Erik Kronberg <erik.a.kronberg@gmail.com>
2011-12-27 00:48:47 +0100

    get-wishlist for code and for id

--- src/wishlistd/models/queries.clj
+++ src/wishlistd/models/queries.clj
@@ -2,14 +2,14 @@
   (:require [clojure.java.jdbc :as sql]))
 
 (def db {:classname "org.postgresql.Driver"
-		 :subprotocol "postgresql"
-		 :subname "//localhost:5432/cwl"
-		 :user "cwl"
-		 :password "wish"})
+         :subprotocol "postgresql"
+         :subname "//localhost:5432/cwl"
+         :user "cwl"
+         :password "wish"})
 ;wishlist
 ;[:id :serial "PRIMARY KEY"]
 ;[:title :varchar "NOT NULL"]
-;[:code :varchar :unique "NOT NULL"]
+;[:code :varchar :unique]
 ;[:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"])))
 
 ;wish
@@ -20,15 +20,21 @@
 ;[:wishlist :int]
 ;["constraint fk_wish_wishlist foreign key(wishlist) references Wishlist(id) on delete cascade"])))
 
-(defn get-wishlist [{:keys [code]}] ; code is unique not null
+(defn get-wishlist-by-code [{:keys [code]}] ; code is unique not null
   (sql/with-connection db
-	(sql/with-query-results results
+    (sql/with-query-results results
       ["select * from wishlist where code=?" code]
+      (first (into [] results)))))
+
+(defn get-wishlist-by-id [{:keys [id]}]
+  (sql/with-connection db
+    (sql/with-query-results results
+      ["select * from wishlist where id=?" id]
       (first (into [] results)))))
 
 (defn insert-wishlist [wishlist]
   (sql/with-connection db
-	(sql/insert-records :wishlist wishlist)))
+    (sql/insert-records :wishlist wishlist)))
 
 (defn delete-wishlist [{:keys [code]}] ; code is unique not null
   (sql/with-connection db
@@ -37,13 +43,13 @@
 (defn update-wishlist [wishlist]
   (let [{:keys [id]} wishlist]
     (sql/with-connection db
-	  (sql/update-values :wishlist ["id=?" id] wishlist))))
+      (sql/update-values :wishlist ["id=?" id] wishlist))))
 
 (defn get-wishes-for-wishlist [{:keys [id]}]
   (sql/with-connection db
     (sql/with-query-results results
-	  ["select * from wish where wishlist=?" id]
-	  (into [] results))))
+      ["select * from wish where wishlist=?" id]
+      (into [] results))))
 
 (defn insert-wish [wish {:keys [id]}]
   (sql/with-connection db
@@ -56,4 +62,4 @@
 (defn update-wishlist [wish]
   (let [{:keys [id]} wish]
     (sql/with-connection db
-	  (sql/update-values :wish ["id=?" id] wish))))
+      (sql/update-values :wish ["id=?" id] wish))))
