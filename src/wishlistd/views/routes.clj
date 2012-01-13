40e4a081df1e3635c73f117bef8972116b85ee27
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-01-13 15:26:14 +0100

    Trying out using query and template from routes

--- src/wishlistd/views/routes.clj
+++ src/wishlistd/views/routes.clj
@@ -1,6 +1,7 @@
 (ns wishlistd.views.routes
-  (:require [wishlistd.views.wishlist :as wishlist])
+  (:require [wishlistd.views.wishlist :as wishlist]
+	        [wishlistd.models.queries :as queries])
   (:use noir.core))
 
 (defpage "/" [] (wishlist/main-page))
-(defpage "/create-wishlist" [] (wishlist/create-wishlist-html))
+(defpage "/create-wishlist" [] (wishlist/wishlist-as-html (queries/create-wishlist-with-wishes)))
