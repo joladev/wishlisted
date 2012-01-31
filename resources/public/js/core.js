def411c1a1dd7dd5449be95869698f968ff80404
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-01-31 17:47:12 +0100

    wishlist save function in js

--- resources/public/js/core.js
+++ resources/public/js/core.js
@@ -1,17 +1,22 @@
 var wishlist = function () {
   var data = null;
-}
+};
 
 wishlist.prototype.load = function(key, callback) {
   var _this = this;
-  $.get("wishlist/" + key, function(data) {
+  $.get("/wishlist/" + key, function (data) {
     _this.data = $.parseJSON(data);
     _this.key = key;
     callback(data);
   });
 };
 
-
+wishlist.prototype.save = function(data, callback) {
+  var _this = this;
+  $.post("/wishlist/", data, function () {
+    // some form of feedback
+  });
+};
 
 var app = function () {
   this.$content = $('div.span10');
