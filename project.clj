b6854f7391c4ae2d658f95ae737aee7700459cbb
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-02-02 23:29:01 +0100

    Added dependency on cheshire for json encoding

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
             :dependencies [[org.clojure/clojure "1.2.1"]
                            [noir "1.1.0"]
                            [postgresql/postgresql "8.4-702.jdbc4"]
-                 		   [org.clojure/java.jdbc "0.1.1"]]
+                 		   [org.clojure/java.jdbc "0.1.1"]
+                 		   [cheshire "2.1.0"]]
             :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
             :main wishlistd.server)
 
