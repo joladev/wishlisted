bd703c168b9c5fb678b0db37b48e91fe71da28ee
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-02-10 14:44:16 +0100

    Added create and read for wishlist redis

--- src/wishlistd/models/redis.clj
+++ src/wishlistd/models/redis.clj
@@ -1,14 +1,28 @@
 (ns wishlistd.models.redis
   (:require [redis.core :as r])
-  (:use wishlistd.code))
+  (:use [wishlistd.code]
+        [cheshire.core]))
   
 (def db {:host "127.0.0.1" :port 6379 :db 0})
     
 (defn new-code []
   "Generates random codes until it finds one that doesn't exist."
   (r/with-server db
-    (let [code (random-code)]
+    (let [code (random-code 8)]
       (if (r/exists code)
-        code
-        (new-code)))))
-      
+        (new-code)
+        code))))
+        
+(defn create-wishlist [wishlist]
+  (let [code (new-code)
+        with-code (assoc wishlist :code code)
+        as-json (encode with-code)]
+    (r/with-server db
+      (r/set code as-json))
+    code))
+
+(defn read-wishlist [code]
+  (r/with-server db
+    (decode
+      (r/get code))))
+
