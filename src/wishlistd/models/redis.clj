1534cd273e3af4bb0a9e5c453307b43bb7fcf905
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-02-10 13:33:49 +0100

    Function to generate random code that doesnt exist already

--- src/wishlistd/models/redis.clj
+++ src/wishlistd/models/redis.clj
@@ -3,3 +3,12 @@
   (:use wishlistd.code))
   
 (def db {:host "127.0.0.1" :port 6379 :db 0})
+    
+(defn new-code []
+  "Generates random codes until it finds one that doesn't exist."
+  (r/with-server db
+    (let [code (random-code)]
+      (if (r/exists code)
+        code
+        (new-code)))))
+      
