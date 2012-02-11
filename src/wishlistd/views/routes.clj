30ce530f5437a5f877d3ce8e137cdea34a325bd8
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-02-11 20:52:09 +0100

    Routing for wish list updates now live and kicking

--- src/wishlistd/views/routes.clj
+++ src/wishlistd/views/routes.clj
@@ -8,8 +8,10 @@
 (defpage "/wishlist/:code" {:keys [code]}
   (wishlist/get-wishlist-json code))
 
-(defpage [:post "/wishlist/"] []
-  (wishlist/create-wishlist-json))
-
+(defpage [:post "/wishlist/"] {:keys [code] :as wishlist}
+  (if (nil? code)
+    (wishlist/create-wishlist-json)
+    (wishlist/update-wishlist-json wishlist)))
+  
 (defpage "/:dontcare" [] ; catchall! kinda...
   (wishlist/main-page))
