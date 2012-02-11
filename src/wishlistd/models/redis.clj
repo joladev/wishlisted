1596a9a51488dce89a8cc0e79b52ad725fcda87f
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-02-11 01:51:45 +0100

    Update wishlist with redis

--- src/wishlistd/models/redis.clj
+++ src/wishlistd/models/redis.clj
@@ -26,5 +26,12 @@
   "Get a single wishlist from datastore."
   (r/with-server db
     (decode
-      (r/get code))))
+      (r/get code)
+      true))) ; so it decodes into keywords
+
+(defn update-wishlist [{:keys [code] :as wishlist}]
+  "Update a single wishlist in the datastore."
+  (let [as-json (encode wishlist)]
+    (r/with-server db
+      (r/set code as-json))))
 
