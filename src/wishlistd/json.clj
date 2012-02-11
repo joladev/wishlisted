301d30ec9be4d651473967642a44c32f8d9db3f4
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-02-11 12:38:09 +0100

    Wrapping cheshire encode and decode

--- src/wishlistd/json.clj
+++ src/wishlistd/json.clj
@@ -1,8 +1,16 @@
 (ns wishlistd.json
-  (:use [cheshire.core]))
+  (:require [cheshire.core :as cheshire]))
 
 (defn json
   "Wraps the response in the json content type and generates JSON from the content. Uses Chesire for date support."
   [content]
   {:headers {"Content-Type" "application/json"}
-   :body (generate-string content)})
+   :body (cheshire/generate-string content)})
+   
+(defn encode [v]
+  "Cheshire encode."
+  (cheshire/encode v))
+  
+(defn decode [v]
+  "Cheshire decode with symbol option set to true."
+  (cheshire/decode v true))
