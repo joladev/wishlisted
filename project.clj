7db36f544cea23c016397c282e9505c95b11fc76
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-02-14 22:09:38 +0100

    Updated project dependencies

--- project.clj
+++ project.clj
@@ -2,8 +2,6 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.2.1"]
                            [noir "1.1.0"]
-                           [postgresql/postgresql "8.4-702.jdbc4"]
-                 		       [org.clojure/java.jdbc "0.1.1"]
                  		       [cheshire "2.1.0"]
                  		       [org.clojars.tavisrudd/redis-clojure "1.3.1-SNAPSHOT"]]
             :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
