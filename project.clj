8db913fcbd4092741289292d9a1bc4834602022a
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-03-01 09:38:23 +0100

    Cleaned up version string

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.2"]
                            [cheshire "2.2.0"]
-                           [org.clojars.tavisrudd/redis-clojure "1.3.1-SNAPSHOT"]]
+                           [org.clojars.tavisrudd/redis-clojure "1.3.1"]]
             :main wishlistd.server)
 
