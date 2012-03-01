b61a9c0baeae847cab54bd1d9e437936b8a1e8a3
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-03-01 09:21:18 +0100

    Sneaky tabs

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.2.1"]
                            [noir "1.1.0"]
-                 		       [cheshire "2.1.0"]
-                 		       [org.clojars.tavisrudd/redis-clojure "1.3.1-SNAPSHOT"]]
+                           [cheshire "2.1.0"]
+                           [org.clojars.tavisrudd/redis-clojure "1.3.1-SNAPSHOT"]]
             :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
             :main wishlistd.server)
 
