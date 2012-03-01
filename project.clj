9cbbfc350fcc6344c38f14d64ebf2f6b3716b958
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-03-01 09:35:46 +0100

    Upgraded to cheshire 2.2.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.2"]
-                           [cheshire "2.1.0"]
+                           [cheshire "2.2.0"]
                            [org.clojars.tavisrudd/redis-clojure "1.3.1-SNAPSHOT"]]
             :main wishlistd.server)
 
