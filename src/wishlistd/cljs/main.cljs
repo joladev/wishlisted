fd271af1f925f64ec1e8f3ce96856f4cf054467c
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-03-01 11:02:07 +0100

    woops

--- src/wishlistd/cljs/main.cljs
+++ src/wishlistd/cljs/main.cljs
@@ -1,10 +1,8 @@
-(ns overtoneinterface.client.main
 (ns wishlistd.client.main
   (:require [crate.core :as crate])
   (:use [jayq.core :only [$ append delegate data]])
   (:use-macros [crate.macros :only [defpartial]]))
 
-(defpartial wish-li [desc url]
 (def $create ($ "#create-wishlist"))
 (def $content ($ "#box"))
 (def $header ($ "#title-box"))
