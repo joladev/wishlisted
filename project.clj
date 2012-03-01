b2f057482e51f3cd0cdafcdeb62c0208d4fb124d
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-03-01 10:03:50 +0100

    ClojureScript dependencies and build options

--- project.clj
+++ project.clj
@@ -3,6 +3,18 @@
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.2"]
                            [cheshire "2.2.0"]
+                           [jayq "0.1.0-SNAPSHOT"]
+                           [crate "0.1.0-SNAPSHOT"]
+                           [fetch "0.1.0-SNAPSHOT"]
                            [org.clojars.tavisrudd/redis-clojure "1.3.1"]]
+            :plugins [[lein-cljsbuild "0.1.2"]]
+            :cljsbuild
+              {:builds
+               [{:source-path "src",
+                 :compiler
+                 {:output-dir "resources/public/cljs/",
+                  :output-to "resources/public/cljs/cljs.js",
+                  :optimizations :simple,
+                  :pretty-print true}}]}
             :main wishlistd.server)
 
