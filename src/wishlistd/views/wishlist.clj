b6280444649150aa451ed229d5e8322be7bd9a2a
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-03-04 10:40:26 +0100

    Updating remotes to use sql backend

--- src/wishlistd/views/wishlist.clj
+++ src/wishlistd/views/wishlist.clj
@@ -1,22 +1,10 @@
 (ns wishlistd.views.wishlist
   (:require [wishlistd.views.common :as common]
-            [wishlistd.json :as response]
-            [wishlistd.models.redis :as model])
+            [wishlistd.models.sql :as model])
   (:use noir.fetch.remotes))
 
 (defn main-page []
   (common/new-layout))
-
-(defn create-wishlist-json [wishlist]
-  (let [wishlist (model/create-wishlist wishlist)]
-    (response/json wishlist)))
-
-(defn get-wishlist-json [code]
-  (let [wishlist (model/read-wishlist code)]
-    (response/json wishlist)))
-
-(defn update-wishlist-json [wishlist]
-  (response/json (model/update-wishlist wishlist)))
 
 (defremote create-wishlist [wishlist]
   (model/create-wishlist wishlist))
@@ -26,3 +14,9 @@
 
 (defremote read-wishlist [code]
   (model/read-wishlist code))
+
+(defremote update-wish [wish]
+  (model/update-wish wish))
+
+(defremote delete-wish [id]
+  (model/delete-wish id))
