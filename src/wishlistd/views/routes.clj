85519459cf462ccf6a9e7acdc3e4660536c0e5a6
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-03-04 11:24:26 +0100

    Removing old code

--- src/wishlistd/views/routes.clj
+++ src/wishlistd/views/routes.clj
@@ -5,13 +5,5 @@
 (defpage "/" []
   (main-page))
 
-(defpage "/wishlist/:code" {:keys [code]}
-  (get-wishlist-json code))
-
-(defpage [:post "/wishlist/"] {:keys [code] :as wishlist}
-  (if (nil? code)
-    (create-wishlist-json wishlist)
-    (update-wishlist-json wishlist)))
-  
 (defpage "/:dontcare" [] ; catchall! kinda...
   (main-page))
