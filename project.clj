38f5c6e7ed899898b64e10e281544499816326f1
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-03-05 09:53:36 +0100

    Remonde old dependencies

--- project.clj
+++ project.clj
@@ -2,13 +2,11 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.3.0-alpha10"]
-                           [cheshire "2.2.0"]
                            [jayq "0.1.0-SNAPSHOT"]
                            [crate "0.1.0-SNAPSHOT"]
                            [fetch "0.1.0-SNAPSHOT"]
                            [korma "0.3.0-beta5"]
-                           [postgresql "9.0-801.jdbc4"]
-                           [org.clojars.tavisrudd/redis-clojure "1.3.1"]]
+                           [postgresql "9.0-801.jdbc4"]]
             :plugins [[lein-cljsbuild "0.1.2"]]
             :cljsbuild
               {:builds
