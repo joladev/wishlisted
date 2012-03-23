89b6cd02c5b145627e03d556cee130ec2047cdd5
Erik Kronberg <erik.a.kronberg@gmail.com>
2012-03-23 18:00:49 +0100

    Updated more dependencies, added log4j.xml
    settings to avoid getting spammed to death with logging

--- project.clj
+++ project.clj
@@ -1,21 +1,25 @@
 (defproject wishlistd "0.1.0-SNAPSHOT"
-            :description "Simple application using Clojure, Noir, ClojureScript, Jayq, Fetch, Crate 
-                          and Korma to handle easy creation of wishlists in the browser."
-            :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.3.0-alpha10"]
-                           [jayq "0.1.0-alpha3"]
-                           [crate "0.1.0-alpha3"]
-                           [fetch "0.1.0-alpha2"]
-                           [korma "0.3.0-beta5"]
-                           [postgresql "9.0-801.jdbc4"]]
-            :plugins [[lein-cljsbuild "0.1.2"]]
-            :cljsbuild
-              {:builds
-               [{:source-path "src",
-                 :compiler
-                 {:output-dir "cljs/",
-                  :output-to "resources/public/cljs/cljs.js",
-                  :optimizations :simple,
-                  :pretty-print true}}]}
-            :main wishlistd.server)
-
+  :description "Simple application using Clojure, Noir, ClojureScript, Jayq, 
+                Fetch, Crate and Korma to handle easy creation of wishlists 
+                in the browser."
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [noir "1.3.0-beta2"]
+                 [jayq "0.1.0-alpha3"]
+                 [crate "0.1.0-alpha3"]
+                 [fetch "0.1.0-alpha2"]
+                 [korma "0.3.0-beta9"]
+                 [postgresql "9.1-901.jdbc4"]
+                 [log4j "1.2.15" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]]
+  :plugins [[lein-cljsbuild "0.1.2"]]
+  :cljsbuild
+    {:builds
+     [{:source-path "src",
+       :compiler
+       {:output-dir "cljs/",
+        :output-to "resources/public/cljs/cljs.js",
+        :optimizations :simple,
+        :pretty-print true}}]}
+  :main wishlistd.server)
